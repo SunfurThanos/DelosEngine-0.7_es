be2ba74f803da8ff7b730f67864757be5648f1e4
Your Name <you@example.com>
2020-06-30 08:59:46 -0430

    sdfsdfsdfsdf

--- samples/2. Manipular archivos/renombrar archivos.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# creando un nuevo archivo
-archivo = "nuevo archivo.txt".path.save("hola mundo").close()
-
-# moviendo archivo creado
-archivo.path.rename("nuevo archivo - renombrado.txt")
-
-# NOTA : sirve tanto para archivos y carpetas
